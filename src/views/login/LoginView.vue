4d9c45dfe10aa6380a77185fc4d58acf61b130fb
Alex Black <ablack00@byu.edu>
2025-02-10 18:27:10 -0700

    milestone 4 first commit

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,7 +1,26 @@
 <template>
-  <button v-if="!isAuthenticated" @click="submitLogin()">Login Page Placeholder Here</button>
+	<h1>Welcome</h1>
+	<v-form class="login" ref="form" v-model="valid" lazy-validation>
+		<v-text-field label="Username" v-model="username" :rules="[required]" />
+		<v-text-field
+			label="Password"
+			v-model="password"
+			:type="'password'"
+			:rules="[eightChar, required]"
+		/>
+		<v-alert v-if="message" :type="messageType">{{ message }}</v-alert>
+		<div class="login-buttons">
+			<v-btn
+				:disabled="!valid"
+				:loading="isAuthenticated"
+				@click="submitLogin()"
+			>
+				Login Here
+			</v-btn>
+			<v-btn @click="forgotPassword()"> Forgot Password </v-btn>
+		</div>
+	</v-form>
 </template>
 <script src="./LoginView.ts" />
-
 
 <style src="./LoginView.scss" />
